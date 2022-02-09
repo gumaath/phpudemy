18d9d09a275901a54dbb26b9034a6929cea8352c
gumaath <guuhsykees@gmail.com>
2022-02-08 22:35:13 -0300

    add delete function

--- classes/Usuario.php
+++ classes/Usuario.php
@@ -108,6 +108,18 @@ Class Usuario {
         ));
     }
 
+    public function delete() {
+        $sql = new Sql();
+        $sql->execQuery("DELETE FROM tb_usuarios WHERE idusuario = :ID", array(
+            ':ID'=>$this->getIdusuario()
+        ));
+
+        $this->setIdusuario(0);
+        $this->setDeslogin("");
+        $this->setDessenha("");
+        $this->setDtcadastro(new DateTime());
+    }
+
     public function __construct($login="", $password="") {
         $this->setDeslogin($login);
         $this->setDessenha($password);
